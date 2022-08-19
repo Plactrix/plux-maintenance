a41b1b54dfae05b3da87701aced9b496dd56d3d1
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-08-18 17:52:18 -0700

    Update fxmanifest.lua
    Updated resource version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Plux Modifications || https://discord.gg/DEQ95eVmQ3"
 description "A simple maintenance mode script"
-version "1.0.1"
+version "1.0.2"
 lua54 "yes"
 
 server_scripts {
