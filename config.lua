52c27018be8627c1614d89ad90b35040df6cac67
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-08-18 17:11:08 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.Command = "maintenancemode" -- The command to toggle maintenance mode
+Config.Permission = "maintenancebypass" -- The ace permission you need to have to bypass maintenance mode
