2d84f492ac0acd4ca3c91598c3da386c218d6c7c
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-08-18 17:19:42 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,6 @@
-Config = {}
-
-Config.Command = "maintenancemode" -- The command to toggle maintenance mode
-Config.Permission = "maintenancebypass" -- The ace permission you need to have to bypass maintenance mode
+Config = {
+    Command = "maintenancemode" -- The command to toggle maintenance mode
+    Permission = "command.maintenancemode" -- The ace permission you need to be able to toggle on and off maintenance mode
+    BypassPermission = "maintenancebypass" -- The ace permission you need to have to bypass maintenance mode
+    MaintenanceModeMsg = "We are currently performing maintenance. Please check our Discord for more information - discord.gg/" -- If maintenance mode is enabled, the player will get disconnected with this message.
+}
