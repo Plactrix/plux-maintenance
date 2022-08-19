960badb6126598c36fc2aca2a55a66bc43c0faba
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-08-18 17:09:35 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "adamant"
+game "gta5"
+
+author "Plux Modifications || https://discord.gg/DEQ95eVmQ3"
+description "A simple maintenance mode script"
+version "1.0.0"
+lua54 "yes"
+
+server_scripts {
+    "config.lua",
+    "server/main.lua"
+}
